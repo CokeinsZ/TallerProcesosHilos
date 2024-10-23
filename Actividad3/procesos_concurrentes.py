9e7d5253a027beb096542176f132469952ad331c
David Toro <davidesteban0736@.com>
2024-10-22 20:56:30 -0500

    se corrigen la actividad de python

--- Actividad3/procesos_concurrentes.py
+++ Actividad3/procesos_concurrentes.py
@@ -1,38 +1,52 @@
-
 import time
 import subprocess
+import multiprocessing
 
-def lanzar_procesos(app,time_app):
-    print("Subproceso ejecutándose...",time_app-1)
-    subprocess.Popen(app)  # Inicia el proceso sin bloquea
-    time.sleep(time_app)
-    print("Subproceso 1 terminado")
+def lanzar_proceso(app,tiempo_espera):
+    print(f"Subproceso lanzando {app}, espera {tiempo_espera} segundos...")
+    subprocess.Popen(app)
+    time.sleep(tiempo_espera)
+    print(f"Subproceso {app} terminado después de {tiempo_espera} segundos.")
 
-def mostrar_lanzados_y_no_lanzados(lanzadas,no_lanzadas):
+def mostrar_lanzados_y_no_lanzados(lanzadas, no_lanzadas):
+    print("\nProcesos lanzados:")
     for lanzada in lanzadas:
-        print("lanzada: ",lanzada)
+        print(lanzada)
 
+    print("\nProcesos no lanzados:")
     for no_lanzada in no_lanzadas:
-        print("no lanzadas: ",no_lanzada)
-    
+        print(no_lanzada)
 
 if __name__ == "__main__":
-    archivo = open("nombres_procesos.txt")
-    texto_archivo=archivo.read()
-    print(texto_archivo)
+    with open("nombres_procesos.txt") as archivo:
+        texto_archivo = archivo.read()
 
-    apps_sin_lanzar=texto_archivo.split(",")
-    apps_lanzadas=[]
+    # Lista de procesos desde el archivo
+    apps_sin_lanzar = texto_archivo.split(",")
+    apps_lanzadas = []
 
-    print(apps_sin_lanzar)
-    # Crear procesos
-    
-    for i in range(0,3,1):
-        lanzar_procesos(apps_sin_lanzar[i],i+1)
-        apps_lanzadas.append(apps_sin_lanzar[i])
-        apps_sin_lanzar.remove(apps_sin_lanzar[i])
+    # Creaamos el proceso padre y lanzamos la primera app
+    padre=multiprocessing.Process(target=lanzar_proceso, args=(apps_sin_lanzar[0], 1))
+    padre.start()
+    padre.join()# Espera a que el proceso padre termine
+    apps_lanzadas.append(apps_sin_lanzar[0])
+    apps_sin_lanzar.pop(0)
 
-    mostrar_lanzados_y_no_lanzados(apps_lanzadas,apps_sin_lanzar)
+    # Crear el primer hijo y lanzamos la segunda app
+    hijo1 = multiprocessing.Process(target=lanzar_proceso, args=(apps_sin_lanzar[0], 2))
+    hijo1.start()
+    hijo1.join()
+    apps_lanzadas.append(apps_sin_lanzar[0])
+    apps_sin_lanzar.pop(0)
 
+    # Crear el segundo hijo y lanzamos la tercera app
+    hijo2=multiprocessing.Process(target=lanzar_proceso, args=(apps_sin_lanzar[0], 3))
+    hijo2.start()
+    hijo2.join()
+    apps_lanzadas.append(apps_sin_lanzar[0])
+    apps_sin_lanzar.pop(0)
 
-    
+    # Crear el tercer hijo y mostramos los procesos lanzados y no lanzados
+    hijo3=multiprocessing.Process(target=mostrar_lanzados_y_no_lanzados, args=(apps_lanzadas, apps_sin_lanzar))
+    hijo3.start()
+    hijo3.join()
